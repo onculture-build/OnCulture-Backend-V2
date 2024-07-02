60d489195256d147572c503e852050b6764e7ee4
ayotunde <ayopedro2012@gmail.com>
2024-07-03 00:19:30 +0100

    setup interceptors

---
+++ src/common/interceptors/error.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AppHelpers } from 'src/utils/helpers';
+
+@Injectable()
+export class ErrorsInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next
+      .handle()
+      .pipe(
+        catchError((err) => throwError(() => AppHelpers.handleException(err))),
+      );
+  }
+}
