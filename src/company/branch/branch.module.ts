3033d9dea70bcecf54ce7bd761624f015bd12059
ayotunde <ayopedro2012@gmail.com>
2024-08-26 10:56:05 +0100

    add controller for branch

--- src/company/branch/branch.module.ts
+++ src/company/branch/branch.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BranchService } from './branch.service';
+import { BranchController } from './branch.controller';
 
 @Module({
-  providers: [BranchService]
+  providers: [BranchService],
+  controllers: [BranchController]
 })
 export class BranchModule {}
