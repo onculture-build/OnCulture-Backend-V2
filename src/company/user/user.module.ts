cac436130a3cec5c3d754ac3da2763371ae22aa4
ayotunde <ayopedro2012@gmail.com>
2024-09-09 22:32:56 +0100

    add email template for welcome to onculture

--- src/company/user/user.module.ts
+++ src/company/user/user.module.ts
@@ -6,6 +6,7 @@ import { CompanyUserQueueConsumer } from '../queue/consumer';
 import { CompanyUserQueueProducer } from '../queue/producer';
 import { EmployeeService } from '../employee/employee.service';
 import { JobRoleService } from '../employee/job-role/job-role.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [BullModule.registerQueue({ name: QUEUE })],
@@ -17,5 +18,6 @@ import { JobRoleService } from '../employee/job-role/job-role.service';
     CompanyUserQueueConsumer,
   ],
   exports: [UserService, CompanyUserQueueProducer, CompanyUserQueueConsumer],
+  controllers: [UserController],
 })
 export class UserModule {}
